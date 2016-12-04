f93d325bdd7c0196b2a20477a649c445c7ccf731
jiangshanmeta <540118044@qq.com>
2016-12-04 14:04:38 +0800

    1

--- javascript/lib.js
+++ javascript/lib.js
@@ -363,3 +363,57 @@ window.requestAnimFrame = (function(){
             window.setTimeout(callback, 1000 / 60);
           };
 })();
+
+var localStorageUtil = {
+  get:function(key){
+    return JSON.parse(localStorage.getItem(key));
+  },
+  set:function(key,value){
+    // set的时候处理成json这样就能存储数组和对象
+    localStorage.setItem(key,JSON.stringify(value));
+  },
+  remove:function(key){
+    if(arguments.length==0){
+      localStorage.clear();
+    }else{
+      localStorage.removeItem(key);
+    }
+    
+  }
+};
+
+var cookieUtil = {
+    get: function (name){
+        var cookieName = encodeURIComponent(name) + "=",
+            cookieStart = document.cookie.indexOf(cookieName),
+            cookieValue = null,
+            cookieEnd; 
+        if (cookieStart > -1){
+            cookieEnd = document.cookie.indexOf(";", cookieStart);
+            if (cookieEnd == -1){
+                cookieEnd = document.cookie.length;
+            }
+            cookieValue = decodeURIComponent(document.cookie.substring(cookieStart + cookieName.length, cookieEnd));
+        }
+        return cookieValue;
+    },
+    set: function (name, value, expires, path, domain, secure) {
+        var cookieText = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+        if (expires instanceof Date) {
+            cookieText += "; expires=" + expires.toGMTString();
+        }
+        if (path) {
+            cookieText += "; path=" + path;
+        }
+        if (domain) {
+            cookieText += "; domain=" + domain;
+        }
+        if (secure) {
+            cookieText += "; secure";
+        }
+        document.cookie = cookieText;
+    },
+    unset: function (name, path, domain, secure){
+        this.set(name, "", new Date(0), path, domain, secure);
+    }
+};
--- php/my_helper.php
+++ php/my_helper.php
@@ -34,4 +34,39 @@ if(!function_exists('get_client_ip')){
 		return $_SERVER['REMOTE_ADDR'];
 	}
 }
+
+if(!function_exists('curl_post')){
+	function curl_post($url,$data){
+		// 对于$data不应该做处理，因为data的格式是要和被请求端约定的，因此这里传入的应该是最终格式
+		
+		$ch = curl_init();// 初始化curl
+
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);// 设置执行后不直接打印出来
+		curl_setopt($ch, CURLOPT_URL, $url);// 设置请求地址
+		curl_setopt($ch, CURLOPT_POST, true);// post方式
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);// post的数据
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);//终止服务器端验证
+
+		$rst = curl_exec($ch);// 执行curl请求
+		
+		curl_close($ch);// 释放句柄
+		return $rst;
+	}
+}
+
+if(!function_exists('curl_get')){
+	function curl_get($url){
+		$ch = curl_init();// 初始化curl
+		
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);// 设置执行后不直接打印出来
+		curl_setopt($ch, CURLOPT_URL, $url);// 设置请求地址
+		curl_setopt($ch, CURLOPT_HEADER, 0);//不返回头信息
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);//终止服务器端验证
+
+		$rst = curl_exec($ch);// 执行curl请求
+		
+		curl_close($ch);// 释放句柄
+		return $rst;
+	}
+}
 ?>
