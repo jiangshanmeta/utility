f3a4c2205ada263ea9d51c455f7e6ae0d9ea51da
jiangshanmeta <540118044@qq.com>
2016-12-12 20:00:59 +0800

    php ci template

---
+++ php/ci/Template.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+// 对ci load view的一个封装
+class Template{
+	public function __construct(){
+		$this->templateData = [];
+		$this->CI = &get_instance();
+	}
+
+	public function set($name,$value){
+		$this->templateData[$name] = $value;
+	}
+
+	public function load($template,$view,$view_data = array(),$return = FALSE){
+		$this->set('contents',$this->CI->load->view($view,$view_data,TRUE));
+		return $this->CI->load->view($template,$this->templateData,$return);
+	}
+}
+?>
