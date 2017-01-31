f6e6122326cada6eea09484da2edaa390c952df9
jiangshanmeta <540118044@qq.com>
2017-01-31 17:07:33 +0800

    widget.js

---
+++ javascript/widget.js
@@ -0,0 +1,45 @@
+// 抽象出公用方法 作为组件类的基类
+function Widget(){
+
+}
+Widget.prototype = {
+	constructor:Widget,
+	// 观察者模式
+	on:function(type,handler){
+		if(!this._handlers){
+			this._handlers = {};
+		}
+		if(!this._handlers[type]){
+			this._handlers[type] = [];
+		}
+		this._handlers[type].push(handler);
+		return this;
+	},
+	fire:function(type,data){
+		if(!this._handlers){
+			this._handlers = {};
+		}
+		if(this._handlers[type]){
+			var handlers = this._handlers[type];
+			for(var i=0,len=handlers.length;i<len;i++){
+				handlers[i](data);
+			}
+		}
+		return this;
+	},
+	remove:function(type,handler){
+		if(!this._handlers){
+			this._handlers = {};
+		}
+		if(this._handlers[type]){
+			handlers = this._handlers[type];
+			for(var i=0,len=handlers.length;i<len;i++){
+				if(handlers[i]===handler){
+					this._handlers[type].splice(i,1);
+					break;
+				}
+			}
+		}
+		return this;
+	},
+}
