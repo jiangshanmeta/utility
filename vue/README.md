529e1316cda65af4214e227dea5c970a98647daf
jiangshanmeta <540118044@qq.com>
2017-07-06 12:33:39 +0800

    vue component vframe

---
+++ vue/README.md
@@ -0,0 +1,10 @@
+## vframe
+
+* src iframe的src
+* ratio iframe的长宽比，默认为1
+
+解决iframe的尺寸(高度)问题
+
+```html
+<vframe src="https://v.qq.com/iframe/player.html?vid=a0511nb4te1&tiny=0&auto=0" :ratio="0.778" ></vframe>
+```
---
+++ vue/v-frame.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="v-frame-container">
+    <div :style="{paddingBottom:ratio*100+'%'}"></div>
+    <iframe :src="src"   class="v-frame-item" allowfullscreen frameborder='0'></iframe>
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        src:{
+            type:String,
+            required:true
+        },
+        ratio:{
+            type:[Number,String],
+            default:1
+        }
+    }
+}
+</script>
+
+<style scoped>
+.v-frame-container{
+    position:relative;
+}
+.v-frame-item{
+    position:absolute;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+}
+</style>
