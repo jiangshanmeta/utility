675633a60878808839220d0af970df10017e8925
jiangshanmeta <540118044@qq.com>
2017-07-18 20:25:40 +0800

    field_tag

---
+++ vue/editor/field_tag.vue
@@ -0,0 +1,36 @@
+<template>
+    <el-checkbox-group v-model="selected">
+        <el-checkbox v-for="item in candidate" :key="item.value" :label="item.value">
+            {{item.label}}
+        </el-checkbox>
+    </el-checkbox-group>
+</template>
+
+<script>
+export default{
+    data (){
+        return {
+            selected:this.value,
+        }
+    },
+    props:{
+        value:{
+            type:Array,
+            required:true,
+        },
+        candidate:{
+            type:Array,
+            required:true,
+        },
+    },
+    watch:{
+        value(newVal){
+            this.selected = newVal;
+
+        },
+        selected(newVal){
+            this.$emit('input',newVal);
+        }
+    },
+}
+</script>
